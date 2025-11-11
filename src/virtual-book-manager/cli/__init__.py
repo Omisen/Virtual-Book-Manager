e90e943dda93a7340cb7e7a95f2f4c59de2a8658
Omisen <simone.conzo@outlook.it>
2025-11-11 09:14:46 +0100

    init repo virtual book manager

---
+++ src/virtual-book-manager/cli/__init__.py
@@ -0,0 +1,15 @@
+#region Imports
+
+#endregion
+
+def mostra_menu():
+    pass
+
+def gestione_input():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/virtual-book-manager/models/libro.py
@@ -0,0 +1,4 @@
+class Libro:
+    def __init__(self):
+        pass
+    
