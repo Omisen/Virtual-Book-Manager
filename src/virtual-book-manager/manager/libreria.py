2dc7d7e7ddb21b423eb5cc9df89e18ce030f66fa
Omisen <simone.conzo@outlook.it>
2025-11-11 18:05:23 +0100

    implemet manager libreria

--- src/virtual-book-manager/manager/libreria.py
+++ src/virtual-book-manager/manager/libreria.py
@@ -8,4 +8,11 @@ class Libreria:
         self.libri: list[Libro] 
         self.utenti: list[Utente]
     
-    
+    def aggiungi_libro(self, libro: Libro) -> None:
+        self.libri.append(libro)
+        
+    def registra_utente(self, utente: Utente) -> None:
+        self.utenti.append(utente)
+        
+    def presta_libro(self, titolo: str, id_utente: int) -> None:
+        pass
